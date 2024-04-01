29b8a6055d14a1922b90eb81d080c27945529207
soheeparklee <soheeparklee@gmail.com>
2024-04-01 16:37:20 +0100

    Initial commit

---
+++ src/main/java/com/github/sc_project01_april2024_versoh/ScProject01April2024VerSohApplication.java
@@ -0,0 +1,13 @@
+package com.github.sc_project01_april2024_versoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScProject01April2024VerSohApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScProject01April2024VerSohApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/sc_project01_april2024_versoh/ScProject01April2024VerSohApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sc_project01_april2024_versoh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScProject01April2024VerSohApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
