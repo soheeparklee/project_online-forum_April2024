97cd0bce76a7f2adf415fe00bee09ae73513a4ce
soheeparklee <soheeparklee@gmail.com>
2024-04-06 15:10:44 +0100

    update, delete comment updated only producer can update, delete

--- src/main/java/com/github/sc_project01_april2024_versoh/service/service/CommentService.java
+++ src/main/java/com/github/sc_project01_april2024_versoh/service/service/CommentService.java
@@ -8,6 +8,7 @@ import com.github.sc_project01_april2024_versoh.repository.user.User;
 import com.github.sc_project01_april2024_versoh.repository.user.UserJpa;
 import com.github.sc_project01_april2024_versoh.repository.userDetails.CustomUserDetails;
 import com.github.sc_project01_april2024_versoh.service.exceptions.NotFoundException;
+import com.github.sc_project01_april2024_versoh.service.exceptions.NotSameUserException;
 import com.github.sc_project01_april2024_versoh.web.DTO.comment.CommentDto;
 import com.github.sc_project01_april2024_versoh.web.DTO.comment.CommentRequest;
 import com.github.sc_project01_april2024_versoh.web.DTO.ResponseDTO;
@@ -50,37 +51,41 @@ public class CommentService {
                 .orElseThrow(()-> new NotFoundException("이메일" + customUserDetails.getEmail() + "을 가진 유저를 찾지 못했습니다."));
         Comment comment= commentJpa.findById(commentId)
                 .orElseThrow(()-> new NotFoundException("해당 아이디"+ commentId + "를 가진 댓글을 찾지 못했습니다."));
-
-        comment.setContent(commentRequest.getContent());
-        commentJpa.save(comment);
-
-
-
-        Post post= postJpa.findById(comment.getPost().getPostId())
-                .orElseThrow(()-> new NotFoundException("해당 아이디를 가진 게시글을 찾지 못했습니다."));
-        List<Comment> comments= commentJpa.findByPost(post);
-        List<CommentDto> commentDtoList= comments
-                .stream()
-                .map(c-> new CommentDto(
-                        c.getCommentId(),
-                        c.getPost().getPostId(),
-                        c.getName(),
-                        c.getContent(),
-                        c.getCreatedAt()))
-                .toList();
+        String nameUser= user.getName();
+        String nameComment= comment.getName();
+        if(nameUser.equals(nameComment)) {
+            comment.setContent(commentRequest.getContent());
+            commentJpa.save(comment);
 
 
-        PostDetailResponse postDetailResponse= PostDetailResponse
-                .builder()
-                .postId(post.getPostId())
-                .title(post.getTitle())
-                .name(post.getName())
-                .content(post.getContent())
-                .createdAt(post.getCreatedAt())
-                .commentDtoList(commentDtoList)
-                .build();
+            Post post = postJpa.findById(comment.getPost().getPostId())
+                    .orElseThrow(() -> new NotFoundException("해당 아이디를 가진 게시글을 찾지 못했습니다."));
+            List<Comment> comments = commentJpa.findByPost(post);
+            List<CommentDto> commentDtoList = comments
+                    .stream()
+                    .map(c -> new CommentDto(
+                            c.getCommentId(),
+                            c.getPost().getPostId(),
+                            c.getName(),
+                            c.getContent(),
+                            c.getCreatedAt()))
+                    .toList();
 
-        return new ResponseDTO(HttpStatus.OK.value(), "Comment update successful", postDetailResponse);
+
+            PostDetailResponse postDetailResponse = PostDetailResponse
+                    .builder()
+                    .postId(post.getPostId())
+                    .title(post.getTitle())
+                    .name(post.getName())
+                    .content(post.getContent())
+                    .createdAt(post.getCreatedAt())
+                    .commentDtoList(commentDtoList)
+                    .build();
+
+            return new ResponseDTO(HttpStatus.OK.value(), "Comment update successful", postDetailResponse);
+        } else {
+            throw new NotSameUserException("Comment update fail. Comment를 작성한 작성자가 아닙니다. ");
+        }
     }
 
     public ResponseDTO deleteComment(CustomUserDetails customUserDetails, Integer commentId) {
@@ -88,8 +93,15 @@ public class CommentService {
                 .orElseThrow(()-> new NotFoundException("이메일" + customUserDetails.getEmail() + "을 가진 유저를 찾지 못했습니다."));
         Comment comment= commentJpa.findById(commentId)
                 .orElseThrow(()-> new NotFoundException("해당 아이디"+ commentId + "를 가진 댓글을 찾지 못했습니다."));
-        commentJpa.delete(comment);
 
-        return new ResponseDTO(HttpStatus.OK.value(), "Comment delete successful");
+        String nameUser= user.getName();
+        String nameComment= comment.getName();
+        if(nameUser.equals(nameComment)) {
+            commentJpa.delete(comment);
+            return new ResponseDTO(HttpStatus.OK.value(), "Comment delete successful");
+        }else{
+            throw new NotSameUserException("Comment delete fail. Comment를 작성한 작성자가 아닙니다.");
+        }
+
     }
 }
